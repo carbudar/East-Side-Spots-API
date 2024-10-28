751854adbdf23766214431912dd49f622504e783
carbudar <carlaannabel26@gmail.com>
2024-10-28 04:03:12 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,76 @@
+import express from 'express';
+import fs from 'fs/promises';
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+let jsonData;
+
+// Load JSON data asynchronously before starting the server
+const readJson = async () => {
+    const data = await fs.readFile('data.json', 'utf-8');
+    jsonData = JSON.parse(data);
+};
+
+readJson().then(() => {
+    app.listen(port, () => {
+        console.log(`App listening on port ${port}`);
+    });
+});
+
+//returns information detail about the chosen place
+app.get('/name/:name', (req, res) => {
+    const placeName = req.params.name.toLowerCase(); //to make it not case sensitive
+    const categories = ['restaurant', 'shop', 'cafe']; //create array for the catrgory parent in the json data
+
+    categories.forEach((category) => { //loop through category
+        if (jsonData[category]) {
+            jsonData[category].forEach((place) => {
+                if (place.name.toLowerCase() === placeName) { //if place name searched matches the placeName in the json,
+                    res.send({ //send these information
+                        name: place.name,
+                        address: place.address,
+                        rating: place.rating,
+                        desc: place.desc
+                    });
+                }
+            });
+        }
+    });
+});
+
+
+//returns array of places based on rating    
+app.get('/rating', (req, res) => {
+    const reqRating = parseFloat(req.query.min); //converts string (rating input) to decimal number
+
+    let ratedPlace = []; //make empty array
+    const categories = ['restaurant', 'shop', 'cafe']; //array of category parent
+
+    categories.forEach((category) => { //loop through all categories
+        if (jsonData[category]) {
+            jsonData[category].forEach((place) => {
+                const placeRating = parseFloat(place.rating);
+                if (placeRating >= reqRating) { //if place from the JSON data has a rating bigger or equals to what was requested,
+                    ratedPlace.push(place.name);//push into the array
+                }
+            });
+        }
+    });
+
+    res.send(ratedPlace);//display to client side
+});
+
+//returns list of name of places tha fits within the category
+app.get('/spot', (req, res) => {
+    const reqCategory = req.query.category; 
+    const categoryList = []; //make empty array
+    
+    if (jsonData[reqCategory]) {
+
+        jsonData[reqCategory].forEach((place) => { //loop through category paren
+        categoryList.push(place.name);//push name into array if match with what was searched
+        });
+        res.send(categoryList); //display it in client side
+    } 
+    });
